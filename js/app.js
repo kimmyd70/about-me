cc78fcbac4651d874d42eb1daff81b05e80019fc
KIMBERLY DAMALAS <kimatcamp@yahoo.com>
2020-03-26 12:31:59 -0700

    styling

--- js/app.js
+++ js/app.js
@@ -1,1 +1,41 @@
 'use strict';
+
+//REMEMBER to comment out console logs
+
+//Display personal welcome
+var name = prompt('Hi. What\'s your name?');
+    console.log('The user\'s name is: ' + name);
+    alert('Hi ' + name + '. Welcome!');
+
+//Ask 5 questions.  Accept y/n or yes/no in caps or lower.  Alert with answer right or wrong
+//Q1 = ready??
+    var name = prompt('Are you ready to play a quiz?');
+    console.log('Are you ready to play a quiz?');
+    alert('Are you ready to play a quiz?');
+
+//Q2 = Favorite color?
+var name = prompt('Take a look at my site. Do you think my favorite color is purple?');
+//conditional
+//accept y/n or yes/no caps and lower
+console.log('The user\'s name is: ' + name);
+alert('Hi ' + name + '! (please click OK as needed)');
+
+//Q3 = Greek name?
+var name = prompt('Do you think my last name is Greek?');
+console.log('The user\'s name is: ' + name);
+alert('Hi ' + name + '! (please click OK as needed)');
+
+//Q4 = Education
+var name = prompt('Does anybody reallllly need 3 Master\'s Degrees?');
+console.log('The user\'s name is: ' + name);
+alert('Hi ' + name + '! (please click OK as needed)');
+
+//Q5 = Goals
+var name = prompt('Work from home is pretty sweet, isn\'t it?');
+console.log('The user\'s name is: ' + name);
+alert('Hi ' + name + '! (please click OK as needed)');
+
+
+//display final message including name
+    console.log('Thanks for playing, ' + name);
+    alert('Thanks for playing, ' + name);
