93a35733601d98fb803a7eb1fa70aca81c7cde5f
KIMBERLY DAMALAS <kimatcamp@yahoo.com>
2020-03-28 20:25:51 -0700

    q7 tested

--- js/app.js
+++ js/app.js
@@ -13,9 +13,8 @@ var correct = 0;
 
 //Ask 5 questions.  Accept y/n or yes/no in caps or lower.  Alert with answer right or wrong
 //q1 = ready?
-var ready;
-var q1 = prompt('Are you ready to play a quiz?');
-ready = q1.toLowerCase();
+
+var ready = prompt('Are you ready to play a quiz?').toLowerCase();
 
 if ((ready === 'y') || (ready === 'yes')){
   //console.log ('Great.  Let\'s start.');
@@ -32,9 +31,8 @@ else{
 }
 
 //q2 = Favorite color?
-var color;
-var q2 = prompt('Take a look at my site. Do you think my favorite color is purple?');
-color = q2.toLowerCase();
+
+var color = prompt('Take a look at my site. Do you think my favorite color is purple?').toLowerCase();
 
 if ((color === 'y') || (color === 'yes')){
   //console.log ('It\'s obvious, isn\'t it!');
@@ -51,9 +49,7 @@ else{
 }
 
 //Q3 = Greek name?
-var greek;
-var q3 = prompt('Do you think my last name is Greek?');
-greek = q3.toLowerCase();
+var greek = prompt('Do you think my last name is Greek?').toLowerCase();
 
 if ((greek === 'y') || (greek === 'yes')){
   //console.log ('Yep. The spelling gives it away');
@@ -70,9 +66,7 @@ else{
 }
 
 //Q4 = Education
-var degree;
-var q4 = prompt('Does anyone realllly need 3 Master\'s degrees?');
-degree = q4.toLowerCase();
+var degree = prompt('Does anyone realllly need 3 Master\'s degrees?').toLowerCase();
 
 if ((degree === 'y') || (degree === 'yes')){
   //console.log ('I know, right?!');
@@ -89,9 +83,7 @@ else{
 }
 
 //Q5 = Goals
-var goal;
-var q5 = prompt('Working from home is pretty sweet, isn\'t it?');
-goal = q5.toLowerCase();
+var goal = prompt('Working from home is pretty sweet, isn\'t it?').toLowerCase();
 
 if ((goal === 'y') || (goal === 'yes')){
   //console.log ('Yoga Pants!!');
@@ -107,7 +99,7 @@ else{
   alert('I give up!');
 }
 
-//Q6 = Guess a number
+//Q6 = Guess a number; 4 attempts
 var guessCount = 1;
 var numRight = false;
 
@@ -115,35 +107,76 @@ while ((guessCount <= 4) && (numRight === false)){
   var secretNum = prompt('Guess a number between 0 and 200');
 
   if ((secretNum < 151) && (secretNum >= 0) && (secretNum !== '')) {
-    console.log ('The secret number is higher.');
+    //console.log ('The secret number is higher.');
     alert ('The secret number is higher.');
   }
-  else if (secretNum === 151){
-    console.log ('Yay. You guessed the secret number!');
+  else if (secretNum === '151'){
+    //console.log ('Yay. You guessed the secret number!');
     alert ('Yay. You guessed the secret number!');
     numRight = true;
     correct ++;
   }
   else if ((secretNum > 151) && (secretNum <= 200) && (secretNum !== '')){
-    console.log ('The secret number is lower.');
+    //console.log ('The secret number is lower.');
     alert ('The secret number is lower.');
   }
   else {
-    console.log ('Please guess a number between 100 and 200');
+    //console.log ('Please guess a number between 100 and 200');
     alert ('Please guess a number between 100 and 200');
 
   }
   guessCount ++;
 }
 
+//wrong show secret number
 if ((guessCount > 4) && (numRight === false)){
-  console.log ('Good try. The secret number is 151.');
+  //console.log ('Good try. The secret number is 151.');
   alert ('Good try. The secret number is 151.');
 }
-else {
-  console.log ('Let\'s move on.');
-  alert ('Let\'s move on.');
+
+//Q7 = guess answer with multiple correct answers; 6 attempts;
+//display all correct answers if correct guess or 6 attempts
+
+var guessCount2 = 1;
+var countryRight = false;
+var countryArr = ['US', 'Germany', 'Greenland', 'England', 'Iraq'];
+
+while ((guessCount2 <= 6) && (countryRight === false)){
+  var country = prompt('Guess a country I\'ve live in').toLowerCase();
+  var index;
+  for (index= 0; index <= 4; index ++){
+  // check array for match to var country
+    if (country === countryArr[index].toLowerCase()){
+      countryRight = true;
+      correct ++;
+    }
+  }
+  if (countryRight === false){
+    //console.log ('Try again.');
+    alert ('Try again.');
+    guessCount2 ++;
+  }
 }
+//wrong--show country list messages
+if ((guessCount2 > 6) && (countryRight === false)){
+  //console.log ('Sorry.  You\'re out of guesses.  The contries are: ');
+  alert ('Sorry.  You\'re out of guesses.  The countries are: ');
+}
+else{
+  //console.log ('Yay. You guessed one! The countries are: ');
+  alert ('Yay. You guessed one! The countries are: ');
+  countryRight = true;
+  correct ++;
+}
+
+//show country list
+var countryList = '';
+for (index = 0; index < countryArr.length; index++){
+  countryList += countryArr[index] + '\n';
+}
+
+//console.log (countryList);
+alert (countryList);
 
 //display final message including name
 //console.log('You got ' + correct + ' out of 7 correct. Thanks for playing, ' + userName);
